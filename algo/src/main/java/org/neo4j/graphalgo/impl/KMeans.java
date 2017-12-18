a46c54776bef5d413644c17972523c95b90e4ae7
Martin Knobloch <mknblch@gmail.com>
2017-12-18 15:12:26 +0100

    WIP

--- algo/src/main/java/org/neo4j/graphalgo/impl/KMeans.java
+++ algo/src/main/java/org/neo4j/graphalgo/impl/KMeans.java
@@ -1,13 +1,17 @@
 package org.neo4j.graphalgo.impl;
 
 import com.carrotsearch.hppc.IntArrayList;
+import com.carrotsearch.hppc.cursors.IntCursor;
 import org.neo4j.graphalgo.api.Graph;
-import org.neo4j.graphalgo.impl.msbfs.BfsConsumer;
-import org.neo4j.graphalgo.impl.msbfs.BfsSources;
-import org.neo4j.graphalgo.impl.msbfs.MultiSourceBFS;
+import org.neo4j.graphalgo.core.utils.ParallelUtil;
+import org.neo4j.graphalgo.core.utils.queue.IntPriorityQueue;
 import org.neo4j.graphdb.Direction;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 /**
  * @author mknblch
@@ -15,15 +19,20 @@ import java.util.Arrays;
 public class KMeans extends Algorithm<KMeans> {
 
     private final Graph graph;
+    private final ExecutorService pool;
+    private final int concurrency;
 
     private final int[] cluster;
-    private final int[] depth;
+    private final int[] distance;
     private final IntArrayList roots;
     private final int nodeCount;
 
-    public KMeans(Graph graph) {
+
+    public KMeans(Graph graph, ExecutorService pool, int concurrency) {
         this.graph = graph;
         nodeCount = (int) graph.nodeCount();
+        this.pool = pool;
+        this.concurrency = concurrency;
         cluster = new int[nodeCount];
         depth = new int[nodeCount];
         Arrays.setAll(cluster, i -> i);
@@ -31,17 +40,52 @@ public class KMeans extends Algorithm<KMeans> {
     }
 
     public KMeans compute(int k) {
-        assignRandom(k);
-        new MultiSourceBFS(graph, graph, Direction.OUTGOING, (nodeId, depth, sourceNodeIds) -> {
+        distributeRandom(k);
 
-        }, roots.toArray());
         return this;
     }
 
-    private void assignRandom(int k) {
+    private void assignClusters() {
+
+        final ArrayList<Runnable> runnables = new ArrayList<>();
+
+        for (IntCursor root : roots) {
+            runnables.add(() -> distance(root.value));
+        }
+        ParallelUtil.runWithConcurrency(concurrency, runnables, pool);
+
+    }
+
+    private void distributeRandom(int k) {
         for (int i = 0; i < k; i++) {
             roots.add((int) (Math.random() * nodeCount));
         }
+    }
+
+    public double[] distance(int startNode) {
+        final IntPriorityQueue queue = IntPriorityQueue.min(nodeCount);
+        final double[] distance = new double[nodeCount];
+        Arrays.fill(distance, Double.POSITIVE_INFINITY);
+        distance[startNode] = 0d;
+        queue.add(startNode, 0d);
+        while (!queue.isEmpty() && running()) {
+            final int node = queue.pop();
+            final double sourceDistance = distance[node];
+            // scan relationships
+            graph.forEachRelationship(
+                    node,
+                    Direction.OUTGOING,
+                    (source, target, relId, weight) -> {
+                        // relax
+                        final double targetDistance = weight + sourceDistance;
+                        if (targetDistance < distance[target]) {
+                            distance[target] = targetDistance;
+                            queue.add(target, targetDistance);
+                        }
+                        return true;
+                    });
+        }
+        return distance;
     }
 
     @Override
